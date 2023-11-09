7bf8c159fcc67a55f9a73ab710ce9a00a0986bf9
leewanjae <dhkswo1@gmail.com>
2023-11-09 15:47:27 +0900

    test: 깃 연동 테스트

--- GreenDefenseForce/HomeViewController.swift
+++ GreenDefenseForce/HomeViewController.swift
@@ -11,7 +11,17 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        view.addSubview(testLabel)
+        testLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        testLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
     }
+    
+    lazy var testLabel: UILabel = {
+        let Label = UILabel()
+        Label.text = "테스트"
+        Label.translatesAutoresizingMaskIntoConstraints = false
+        return Label
+    }()
 }
 
