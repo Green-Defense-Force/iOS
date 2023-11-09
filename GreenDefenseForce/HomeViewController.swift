bffd8b9bfca722129db052f5035b8c7769bafed0
leewanjae <dhkswo1@gmail.com>
2023-11-09 15:35:57 +0900

    Initial Commit

---
+++ GreenDefenseForce/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GreenDefenseForce
+//
+//  Created by 이완재 on 11/9/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
+
