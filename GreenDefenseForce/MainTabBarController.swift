9173daa97a517abee46da73ee7d3bf5bc1cb9dcf
leewanjae <dhkswo1@gmail.com>
2023-11-10 20:06:47 +0900

    feat: 메인탭바

---
+++ GreenDefenseForce/MainTabBarController.swift
@@ -0,0 +1,37 @@
+//
+//  MainTabBarController.swift
+//  GreenDefenseForce
+//
+//  Created by 이완재 on 11/10/23.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MainTabBarController - viewDidLoad() called")
+
+        let challengeNC = UINavigationController(rootViewController: ChallengeViewController())
+        let ploggingNC = UINavigationController(rootViewController: PloggingViewController())
+        let homeNC = UINavigationController(rootViewController: HomeViewController())
+        let storeNC = UINavigationController(rootViewController: StoreViewController())
+        let profilNC = UINavigationController(rootViewController: ProfilViewController())
+        
+        self.viewControllers = [challengeNC, ploggingNC, homeNC, storeNC, profilNC]
+        
+        let challengeTabBarItem = UITabBarItem(title: "챌린지", image: UIImage(systemName: "flag.2.crossed"), tag: 0)
+        let ploggingTabBarItem = UITabBarItem(title: "플로깅", image: UIImage(systemName: "shoe"), tag: 1)
+        let homeTabBarItem = UITabBarItem(title: "게임", image: UIImage(systemName: "gamecontroller"), tag: 2)
+        let storeTabBarItem = UITabBarItem(title: "스토어", image: UIImage(systemName: "storefront"), tag: 3)
+        let profilTabBarItem = UITabBarItem(title: "마이페이지", image: UIImage(systemName: "person"), tag: 4)
+        
+        challengeNC.tabBarItem = challengeTabBarItem
+        ploggingNC.tabBarItem = ploggingTabBarItem
+        homeNC.tabBarItem = homeTabBarItem
+        storeNC.tabBarItem = storeTabBarItem
+        profilNC.tabBarItem = profilTabBarItem
+    }
+}
--- GreenDefenseForce/SceneDelegate.swift
+++ GreenDefenseForce/SceneDelegate.swift
@@ -12,28 +12,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
-        let mainViewController = HomeViewController()
+        let mainViewController = MainTabBarController()
         
         window?.rootViewController = mainViewController
         window?.makeKeyAndVisible()
-    }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
     }
 }
 
