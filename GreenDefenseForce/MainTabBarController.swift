5a90a66b2658c874561fa596d47e109dc35270c2
leewanjae <dhkswo1@gmail.com>
2023-11-12 22:33:53 +0900

    feat: 탭바 색 변경

--- GreenDefenseForce/MainTabBarController.swift
+++ GreenDefenseForce/MainTabBarController.swift
@@ -13,7 +13,7 @@ class MainTabBarController: UITabBarController {
         super.viewDidLoad()
         
         print("MainTabBarController - viewDidLoad() called")
-
+        
         let challengeNC = UINavigationController(rootViewController: ChallengeViewController())
         let ploggingNC = UINavigationController(rootViewController: PloggingViewController())
         let homeNC = UINavigationController(rootViewController: HomeViewController())
@@ -23,10 +23,16 @@ class MainTabBarController: UITabBarController {
         self.viewControllers = [challengeNC, ploggingNC, homeNC, storeNC, profilNC]
         
         let challengeTabBarItem = UITabBarItem(title: "챌린지", image: UIImage(systemName: "flag.2.crossed"), tag: 0)
+        
         let ploggingTabBarItem = UITabBarItem(title: "플로깅", image: UIImage(systemName: "shoe"), tag: 1)
         let homeTabBarItem = UITabBarItem(title: "게임", image: UIImage(systemName: "gamecontroller"), tag: 2)
         let storeTabBarItem = UITabBarItem(title: "스토어", image: UIImage(systemName: "storefront"), tag: 3)
         let profilTabBarItem = UITabBarItem(title: "마이페이지", image: UIImage(systemName: "person"), tag: 4)
+        
+        tabBar.tintColor = .white
+        tabBar.backgroundColor = .black
+        
+        
         
         challengeNC.tabBarItem = challengeTabBarItem
         ploggingNC.tabBarItem = ploggingTabBarItem
