99f1f225b482314a5b93b93ee2beb2951e7456d0
leewanjae <dhkswo1@gmail.com>
2023-11-12 22:33:15 +0900

    feat: 서버에서 이미지 받기 Test

--- GreenDefenseForce/Home/HomeViewController.swift
+++ GreenDefenseForce/Home/HomeViewController.swift
@@ -6,21 +6,39 @@
 //
 
 import UIKit
+import Combine
 
 class HomeViewController: UIViewController {
+    
+    var imageView: UIImageView!
+    var cancellable: AnyCancellable?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.setNavigationBlackTitleWhiteBg(title: "게임")
+        nvLeftItem(image: UIImage(systemName: "arrow.left")!, action: #selector(backTappedButton), tintColor: .black)
+        
+        imageView = UIImageView(frame: self.view.bounds)
+        imageView.contentMode = .scaleAspectFit
+        self.view.addSubview(imageView)
+
+        if let imageURL = URL(string: "https://i.pravatar.cc/") {
+            cancellable = URLSession.shared.dataTaskPublisher(for: imageURL)
+                .map { UIImage(data: $0.data)}
+                .receive(on: DispatchQueue.main)
+                .sink(receiveCompletion: { completion in
+                    if case .failure(let err) = completion {
+                        print("Retrieval failed: \(err)")
+                    }
+                }, receiveValue: { [weak self] image in
+                    if let image = image {
+                        self?.imageView.image = image
+                    } else {
+                        print("이미지 변환 실패")
+                    }
+                })
+        }
     }
 }
 
-extension UIViewController {
-    func setNavigationBlackTitleWhiteBg(title: String) {
-        view.backgroundColor = .white
-        navigationItem.title = title
-        navigationItem.largeTitleDisplayMode = .always
-        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
-    }
-}
