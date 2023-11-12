f6a978ffd96faf81a6b3b91901b25b3da3c89925
leewanjae <dhkswo1@gmail.com>
2023-11-12 22:32:18 +0900

    feat: UIViewController_Extension 파일추가

---
+++ GreenDefenseForce/Util/UIViewController+Extension.swift
@@ -0,0 +1,28 @@
+//
+//  Extension.swift
+//  GreenDefenseForce
+//
+//  Created by 이완재 on 11/12/23.
+//
+
+import UIKit
+
+extension UIViewController {
+    func setNavigationBlackTitleWhiteBg(title: String) {
+        view.backgroundColor = .white
+        navigationItem.title = title
+        navigationItem.largeTitleDisplayMode = .always
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
+    }
+
+    func nvLeftItem(image: UIImage, action: Selector , tintColor: UIColor) {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: action)
+        navigationItem.leftBarButtonItem?.tintColor = tintColor
+    }
+    
+    @objc func backTappedButton() {
+        navigationController?.popViewController(animated: true)
+        print("뒤로가기")
+    }
+}
+
