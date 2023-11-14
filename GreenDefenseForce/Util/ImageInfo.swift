9faca06e31d2b36acb76134c2c75de0309b02bd8
leewanjae <dhkswo1@gmail.com>
2023-11-14 19:16:45 +0900

    feat: imageInfo

--- GreenDefenseForce/Util/ImageInfo.swift
+++ GreenDefenseForce/Util/ImageInfo.swift
@@ -5,23 +5,9 @@
 //  Created by 이완재 on 11/14/23.
 //
 
-import Foundation
+import UIKit
 
-struct GitHubFile: Codable {
-    let payload: Payload
-    
-    struct Payload: Codable {
-        let tree: Tree
-    }
-    
-    struct Tree: Codable {
-        let items: [TreeItem]
-    }
-    
-    struct TreeItem: Codable {
-        let name: String
-        let path: String
-        let contentType: String
-    }
-    
+struct ImageInfo {
+    let index: Int
+    let image: UIImage
 }
