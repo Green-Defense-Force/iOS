442c5bf947b49ea9e3d21b11a5b977625ed83b6b
leewanjae <dhkswo1@gmail.com>
2023-11-15 19:14:28 +0900

    refactor: url수정

--- GreenDefenseForce/Home/HomeViewModel.swift
+++ GreenDefenseForce/Home/HomeViewModel.swift
@@ -20,7 +20,7 @@ class HomeViewModel {
         let imageURLs = [
             "https://github.com/leewanjae/imageAPI_Test/blob/main/image/66px_5zu1_230310.jpg?raw=true",
             "https://github.com/leewanjae/imageAPI_Test/blob/main/image/New%20Piskel.png?raw=true",
-            "https://github.com/leewanjae/imageAPI_Test/blob/main/image/monster1.png?raw=true",
+            "https://github.com/leewanjae/imageAPI_Test/blob/main/image/monster1%20(2).png?raw=true",
             "https://github.com/leewanjae/imageAPI_Test/blob/main/image/New%20Piskel%20(2).png?raw=true",
         ]
         
@@ -28,7 +28,6 @@ class HomeViewModel {
             imageFetcher.imageFetch(url: url)
                 .map { ImageInfo(index: index, image: $0)}
         }
-        
         Publishers.MergeMany(publishers)
             .collect()
             .map { $0.sorted(by: { $0.index < $1.index }).map { $0.image } }
