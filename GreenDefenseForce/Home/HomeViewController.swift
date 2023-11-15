3178d37330bc42ae9d00051210c385c92f5f223f
leewanjae <dhkswo1@gmail.com>
2023-11-16 01:00:11 +0900

    feat: 포켓몬배틀 배치구조 테스트

--- GreenDefenseForce/Home/HomeViewController.swift
+++ GreenDefenseForce/Home/HomeViewController.swift
@@ -1,4 +1,3 @@
-//
 //  HomeViewController.swift
 //  GreenDefenseForce
 //
@@ -24,18 +23,6 @@ class HomeViewController: UIViewController {
         field.addSubview(imageView[2])
         field.addSubview(imageView[3])
         
-        let label = UILabel()
-        label.text = "한강에 버려진 쓰레기: Lv0"
-        label.textColor = .black
-        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        hpBar.addSubview(label)
-        
-        NSLayoutConstraint.activate([
-            label.leadingAnchor.constraint(equalTo: hpBar.leadingAnchor, constant: 5),
-            label.topAnchor.constraint(equalTo: hpBar.topAnchor, constant: -30)
-        ])
-        
         return field
     }()
     
@@ -44,10 +31,36 @@ class HomeViewController: UIViewController {
         hpBar.translatesAutoresizingMaskIntoConstraints = false
         hpBar.progressTintColor = .red
         hpBar.progressViewStyle = .default
-        hpBar.progress = 1
         hpBar.layer.borderColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1)
         hpBar.layer.borderWidth = 5
+        hpBar.progress = 1
+        
+        let label = UILabel()
+        label.text = "쓰레기 봉투: LV1"
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        hpBar.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: hpBar.leadingAnchor),
+            label.topAnchor.constraint(equalTo: hpBar.topAnchor, constant: -30)
+        ])
+        
         return hpBar
+    }()
+    
+    lazy var attackButton: UIButton = {
+       let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("공격", for: .normal)
+        button.titleLabel?.adjustsFontSizeToFitWidth = true
+        button.backgroundColor = .red
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 10
+        button.layer.borderWidth = 5
+        button.layer.borderColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1)
+        return button
     }()
     
     override func viewDidLoad() {
@@ -61,7 +74,7 @@ class HomeViewController: UIViewController {
     func setUI() {
         
         // NVTitle
-        setNavigationBlackTitleWhiteBg(title: "게임")
+        setNavigationBlackTitleWhiteBg(title: "")
         // NVLItem
         nvLeftItem(image: UIImage(systemName: "arrow.left")!, action: #selector(backTappedButton), tintColor: .black)
         
@@ -82,7 +95,7 @@ class HomeViewController: UIViewController {
         stackView.axis = .vertical
         stackView.addArrangedSubview(imageView[0])
         stackView.addArrangedSubview(field)
-        stackView.distribution = .fill
+        stackView.distribution = .fillEqually
         NSLayoutConstraint.activate([
             stackView.topAnchor.constraint(equalTo: view.topAnchor),
             stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
@@ -109,10 +122,10 @@ class HomeViewController: UIViewController {
         // 유저 캐릭터
         imageView[3].alpha = 0.0
         NSLayoutConstraint.activate([
-            imageView[1].widthAnchor.constraint(equalToConstant: 150),
-            imageView[1].heightAnchor.constraint(equalToConstant: 150),
-            imageView[1].bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -120),
-            imageView[1].leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            imageView[1].widthAnchor.constraint(equalToConstant: 320),
+            imageView[1].heightAnchor.constraint(equalToConstant: 320),
+            imageView[1].bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            imageView[1].leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -40),
             
             imageView[3].widthAnchor.constraint(equalTo: imageView[1].widthAnchor),
             imageView[3].heightAnchor.constraint(equalTo: imageView[1].heightAnchor),
@@ -123,19 +136,29 @@ class HomeViewController: UIViewController {
         
         // 몬스터
         NSLayoutConstraint.activate([
-            imageView[2].bottomAnchor.constraint(equalTo: imageView[1].bottomAnchor),
+            imageView[2].centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100),
             imageView[2].trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 30),
-            imageView[2].widthAnchor.constraint(equalToConstant: 300),
-            imageView[2].heightAnchor.constraint(equalToConstant: 300),
+            imageView[2].widthAnchor.constraint(equalToConstant: 320),
+            imageView[2].heightAnchor.constraint(equalToConstant: 320),
         ])
         
         // Hp
         view.addSubview(hpBar)
         NSLayoutConstraint.activate([
             hpBar.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            hpBar.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            hpBar.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -10),
+            hpBar.centerYAnchor.constraint(equalTo: imageView[2].centerYAnchor),
+            hpBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            hpBar.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.4),
             hpBar.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        
+        // 공격버튼
+        view.addSubview(attackButton)
+        NSLayoutConstraint.activate([
+            attackButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 120),
+            attackButton.centerYAnchor.constraint(equalTo: imageView[1].centerYAnchor),
+            attackButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.3),
+            attackButton.heightAnchor.constraint(equalToConstant: 80)
         ])
     }
     
@@ -148,10 +171,6 @@ class HomeViewController: UIViewController {
                 }
             }
             .store(in: &subscriptions)
-    }
-    
-    func topAnchorForHpBar() -> NSLayoutYAxisAnchor {
-        return imageView[0].topAnchor
     }
     
     func attackMonster() {
@@ -181,4 +200,3 @@ class HomeViewController: UIViewController {
         hpBar.setProgress(currentProgress, animated: true)
     }
 }
-
