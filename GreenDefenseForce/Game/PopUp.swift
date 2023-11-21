e1529cdd2a67e392758684eed7e04b471293f220
leewanjae <dhkswo1@gmail.com>
2023-11-21 17:14:07 +0900

    feat: 팝업창 컨텐츠 추가

--- GreenDefenseForce/Game/PopUp.swift
+++ GreenDefenseForce/Game/PopUp.swift
@@ -24,20 +24,31 @@ final class PopUp: UIViewController {
         return popUp
     }()
     
-    lazy var textContent: UILabel = {
+    lazy var titleLabel: UILabel = {
        let label = UILabel()
-        label.text = "내용"
-        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.text = "재활용 성공!!!!!"
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 30)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var contentLable: UILabel = {
+        let label = UILabel()
+        label.text = "팔드로 돌아갑니다."
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 20)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     lazy var closeButton: UIButton = {
         let button = UIButton()
-        button.setTitle("돌아가기", for: .normal)
-        button.setTitleColor(.black, for: .normal)
+        button.setTitle("확인", for: .normal)
+        button.setTitleColor(.white, for: .normal)
         button.layer.borderColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1)
         button.layer.borderWidth = 5
+        button.backgroundColor = .black
         button.addTarget(self, action: #selector(closeButtonTapped), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
@@ -58,18 +69,25 @@ final class PopUp: UIViewController {
             popUp.heightAnchor.constraint(equalToConstant: 200)
         ])
         
-        popUp.addSubview(textContent)
+        popUp.addSubview(titleLabel)
         NSLayoutConstraint.activate([
-            textContent.topAnchor.constraint(equalTo: popUp.topAnchor, constant: 20),
-            textContent.centerXAnchor.constraint(equalTo: popUp.centerXAnchor)
+            titleLabel.topAnchor.constraint(equalTo: popUp.topAnchor, constant: 20),
+            titleLabel.centerXAnchor.constraint(equalTo: popUp.centerXAnchor)
         ])
-        
+
         popUp.addSubview(closeButton)
         NSLayoutConstraint.activate([
-            closeButton.bottomAnchor.constraint(equalTo: popUp.bottomAnchor),
+            closeButton.bottomAnchor.constraint(equalTo: popUp.bottomAnchor, constant: -30),
             closeButton.centerXAnchor.constraint(equalTo: popUp.centerXAnchor),
             closeButton.widthAnchor.constraint(equalToConstant: 100),
             closeButton.heightAnchor.constraint(equalToConstant: 50),
+        ])
+        
+        popUp.addSubview(contentLable)
+        NSLayoutConstraint.activate([
+            contentLable.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+            contentLable.bottomAnchor.constraint(equalTo: closeButton.topAnchor, constant: -10),
+            contentLable.centerXAnchor.constraint(equalTo: popUp.centerXAnchor  )
         ])
     }
     
