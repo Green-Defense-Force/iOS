11d03586ce37e8cbd82c6ce758a35c99aaffdfe5
leewanjae <dhkswo1@gmail.com>
2023-11-21 17:11:29 +0900

    feat: Model 추가

---
+++ GreenDefenseForce/Model/MapModel.swift
@@ -0,0 +1,17 @@
+//
+//  Model.swift
+//  GreenDefenseForce
+//
+//  Created by 이완재 on 11/20/23.
+//
+
+import UIKit
+
+struct MapModel  {
+    var ticketNum: Int
+    var coinNum: Int
+    var mapMonsters: [UIImageView]
+    var character: [UIImageView]
+    var userName: String
+    var userLevel: Int
+}
