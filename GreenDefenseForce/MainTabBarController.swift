373b5c5181a5e9f5328f550cb18f00f23b84a725
leewanjae <dhkswo1@gmail.com>
2023-11-27 13:04:33 +0900

    refactor: 탭바 수정

--- GreenDefenseForce/MainTabBarController.swift
+++ GreenDefenseForce/MainTabBarController.swift
@@ -13,31 +13,23 @@ class MainTabBarController: UITabBarController {
         super.viewDidLoad()
         
         let challengeNC = UINavigationController(rootViewController: ChallengeViewController())
-        let ploggingNC = UINavigationController(rootViewController: PloggingViewController())
         let homeNC = UINavigationController(rootViewController: HomeViewController())
-        let storeNC = UINavigationController(rootViewController: StoreViewController())
-        let profilNC = UINavigationController(rootViewController: ProfilViewController())
+        let ploggingNC = UINavigationController(rootViewController: PloggingViewController())
         
-        self.viewControllers = [challengeNC, ploggingNC, homeNC, storeNC, profilNC]
+        self.viewControllers = [challengeNC, homeNC, ploggingNC]
         
         let challengeTabBarItem = UITabBarItem(title: "챌린지", image: UIImage(systemName: "flag.2.crossed")?.withTintColor(.lightGray, renderingMode: .alwaysOriginal), tag: 0)
-        let ploggingTabBarItem = UITabBarItem(title: "플로깅", image: UIImage(systemName: "figure.walk")?.withTintColor(.lightGray, renderingMode: .alwaysOriginal), tag: 1)
         let homeTabBarItem = UITabBarItem(title: "메인", image: UIImage(systemName: "house")?.withTintColor(.lightGray, renderingMode: .alwaysOriginal), tag: 2)
-        let storeTabBarItem = UITabBarItem(title: "스토어", image: UIImage(systemName: "shippingbox")?.withTintColor(.lightGray, renderingMode: .alwaysOriginal), tag: 3)
-        let profilTabBarItem = UITabBarItem(title: "마이페이지", image: UIImage(systemName: "person")?.withTintColor(.lightGray, renderingMode: .alwaysOriginal), tag: 4)
+        let ploggingTabBarItem = UITabBarItem(title: "플로깅", image: UIImage(systemName: "figure.walk")?.withTintColor(.lightGray, renderingMode: .alwaysOriginal), tag: 1)
         
         tabBar.backgroundColor = .white
         tabBar.tintColor = .lightGray
         tabBar.barTintColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.7)
-               
-        
         
         challengeNC.tabBarItem = challengeTabBarItem
-        ploggingNC.tabBarItem = ploggingTabBarItem
         homeNC.tabBarItem = homeTabBarItem
-        storeNC.tabBarItem = storeTabBarItem
-        profilNC.tabBarItem = profilTabBarItem
+        ploggingNC.tabBarItem = ploggingTabBarItem
         
-        self.selectedIndex = 2
+        self.selectedIndex = 1
     }
 }
--- GreenDefenseForce/MyPage/MyPageViewController.swift
+++ GreenDefenseForce/MyPage/MyPageViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ProfilViewController: UIViewController {
+class MyPageViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
