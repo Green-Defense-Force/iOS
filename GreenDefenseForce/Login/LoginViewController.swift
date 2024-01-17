afe8495fcdab37c6b2bdff58ab2f383f5c2c5e37
leewanjae <dhkswo1@gmail.com>
2024-01-17 14:58:23 +0900

    fix: 로그인 UI

--- GreenDefenseForce/Login/LoginViewController.swift
+++ GreenDefenseForce/Login/LoginViewController.swift
@@ -15,112 +15,100 @@ import FirebaseAuth
 import GoogleSignIn
 
 class LoginViewController: UIViewController {
-    let loginLogo: UIImageView = {
+    let container: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    let logoImage: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(named: "loginLogo")
-        image.translatesAutoresizingMaskIntoConstraints = false
-        
-        let logoTitle: UILabel = {
-            let label = UILabel()
-            label.translatesAutoresizingMaskIntoConstraints = false
-            label.text = "도전! 환경 지킴 방범대"
-            label.font = .systemFont(ofSize: 30, weight: .bold)
-            return label
-        }()
-        
-        let loginLabel: UILabel = {
-            let label = UILabel()
-            label.translatesAutoresizingMaskIntoConstraints = false
-            label.text = "소셜 로그인"
-            label.font = .systemFont(ofSize: 20, weight: .medium)
-            return label
-        }()
-        
-        image.addSubview(logoTitle)
-        image.addSubview(loginLabel)
-        NSLayoutConstraint.activate([
-            logoTitle.topAnchor.constraint(equalTo: image.bottomAnchor, constant: 30),
-            logoTitle.centerXAnchor.constraint(equalTo: image.centerXAnchor),
-            loginLabel.topAnchor.constraint(equalTo: logoTitle.bottomAnchor, constant: 50),
-            loginLabel.centerXAnchor.constraint(equalTo: logoTitle.centerXAnchor)
-        ])
+        image.accessibilityLabel = "로고 이미지"
+        image.contentMode = .scaleAspectFit // 이미지 컨텐트 모드 설정
         return image
     }()
     
-   
-    
-    let btnContainer: UIStackView = {
-        let stackView = UIStackView()
-        stackView.spacing = 20
-        stackView.axis = .vertical
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.contentMode = .scaleAspectFit
-        return stackView
+    let gdfTitle: UILabel = {
+        let label = UILabel()
+        label.text = "도전! 환경 지킴 방범대"
+        label.font = .systemFont(ofSize: 30, weight: .bold)
+        return label
     }()
-   
+    
+    let loginLabel: UILabel = {
+        let label = UILabel()
+        label.text = "소셜 로그인"
+        label.font = .systemFont(ofSize: 20, weight: .medium)
+        return label
+    }()
+    
+    let btnContainer: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
     lazy var kakaoAuthVM: KakaoAuthVM = {
-            let vm = KakaoAuthVM()
-            vm.onLoginSuccess = {
-                self.switchToMainScreen()
-            }
-            return vm
-        }()
-        
-        lazy var googleAuthVM: GoogleAuthVM = {
-            let vm = GoogleAuthVM()
-            vm.onLoginSuccess = {
-                self.switchToMainScreen()
-            }
-            return vm
-        }()
+        let vm = KakaoAuthVM()
+        vm.onLoginSuccess = {
+            self.switchToMainScreen()
+        }
+        return vm
+    }()
+    
+    lazy var googleAuthVM: GoogleAuthVM = {
+        let vm = GoogleAuthVM()
+        vm.onLoginSuccess = {
+            self.switchToMainScreen()
+        }
+        return vm
+    }()
     
     lazy var kakaoLoginBtn: UIButton = {
         let button = UIButton()
         button.setBackgroundImage(UIImage(named: "kakaoLogin"), for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
         button.addTarget(self, action: #selector(kakaoLoginBtnClicked), for: .touchUpInside)
         return button
     }()
     
-    
-    lazy var googleLoginBtn: UIButton = {
-        let button = UIButton()
-        button.setBackgroundImage(UIImage(named: "googleLogin"), for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
+    lazy var googleLoginBtn: GIDSignInButton = {
+        let button = GIDSignInButton()
+        button.style = .wide
         button.addTarget(self, action: #selector(googleLoginBtnClicked), for: .touchUpInside)
         return button
     }()
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
-        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        container.pin.center().width(80%).height(70%)
+        container.flex.layout()
     }
     
     private func setUI() {
         view.backgroundColor = .white
-        view.addSubview(loginLogo)
-        view.addSubview(btnContainer)
-        
-        btnContainer.addArrangedSubview(kakaoLoginBtn)
-        btnContainer.addArrangedSubview(googleLoginBtn)
-        
-        NSLayoutConstraint.activate([
-            loginLogo.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            loginLogo.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -200),
-            btnContainer.topAnchor.constraint(equalTo: loginLogo.bottomAnchor, constant: 200),
-            btnContainer.centerXAnchor.constraint(equalTo: loginLogo.centerXAnchor)
-        ])
+        btnContainer.flex.direction(.column).alignItems(.center).define { flex in
+            flex.addItem(googleLoginBtn).width(250).height(45).marginBottom(10)
+            flex.addItem(kakaoLoginBtn).width(250).height(45)
+        }
+        container.flex.direction(.column).alignItems(.center).define { flex in
+            flex.addItem(logoImage).height(200).margin(20)
+            flex.addItem(gdfTitle).marginBottom(10)
+            flex.addItem(loginLabel).marginBottom(30)
+            flex.addItem(btnContainer)
+        }
+        view.addSubview(container)
     }
     
     private func switchToMainScreen() {
-            if let sceneDelegate = self.view.window?.windowScene?.delegate as? SceneDelegate {
-                let mainVC = CustomTabBarViewController()
-                sceneDelegate.changeRootViewController(to: mainVC, animated: true)
-            }
+        if let sceneDelegate = self.view.window?.windowScene?.delegate as? SceneDelegate {
+            let mainVC = CustomTabBarViewController()
+            sceneDelegate.changeRootViewController(to: mainVC, animated: true)
         }
+    }
     
     @objc func kakaoLoginBtnClicked() {
         print("로그인 버튼 클릭")
